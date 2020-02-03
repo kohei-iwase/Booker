2131329cd2dd7f3f076d035360544084602088ab
kohei-iwase <me.tekelily@gmail.com>
2020-02-03 09:26:32 +0000

    [Create]showpage

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+	def top
+	end
+	#booksページの定義、新規投稿機能と一覧機能
+	def books
+		#投稿機能
+		@book = Book.new
+		#一覧機能
+		@books = Book.all
+	end
+	#新規投稿機能の定義
+	def create
+		#　ストロングパラメーターのため＠なし
+		book = Book.new(book_params)
+		#bookをDBへ保存
+		book.save
+		#リダイレクト先をとりあえずbooksへ
+		redirect_to '/books'
+	end
+	#詳細ページの定義
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	#ストロングパラメーター
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/new'
+  get'top' => 'books#top'
+  get'books' => 'books#books'
+  post'books' => 'books#create'
+
+
 end
