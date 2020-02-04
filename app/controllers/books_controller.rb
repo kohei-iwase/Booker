1bf3f322da0afbfa350918d25cdf2bc50daf9298
kohei-iwase <me.tekelily@gmail.com>
2020-02-04 04:50:14 +0000

    [Create]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,22 +11,33 @@ class BooksController < ApplicationController
 	#新規投稿機能の定義
 	def create
 		#　ストロングパラメーターのため＠なし
-		book = Book.new(book_params)
-		#bookをDBへ保存
-		book.save
+		@book = Book.new(book_params)
+		#bookをDBへ保存が成功したら
+		if @book.save
 		#リダイレクト先をとりあえずbooksへ
 		redirect_to '/books'
+		else
+		render :books
+		end
 	end
 	#詳細ページの定義
 	def show
 		@book = Book.find(params[:id])
 	end
+	#編集ページの定義
 	def edit
 		@book = Book.find(params[:id])
 	end
+	#アップデート機能の定義
 	def update
 		book = Book.find(params[:id])
 		book.update(book_params)
+		redirect_to book_path(book.id)
+	end
+	#削除機能
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
 		redirect_to '/books'
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'books#top'
   get'books/new'
-  get'top' => 'books#top'
   get'books' => 'books#books'
   post'books' => 'books#create'
 
@@ -9,5 +9,7 @@ get 'books/:id' => 'books#show', as: 'book'
 get 'books/:id/edit' => 'books#edit', as:'edit_book'
 
 patch 'books/:id' => 'books#update', as: 'update_book'
+
+delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
 end
